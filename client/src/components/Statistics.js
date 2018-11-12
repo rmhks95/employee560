204c66c068a471a41653f4d8fe422f9525cb3b44
Ryan Huse <rmhks95@ksu.edu>
2018-11-12 14:08:12 -0600

    added statistics

---
+++ client/src/components/Statistics.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Stats extends Component {
+  // Initialize the state
+  constructor(props){
+    super(props);
+    this.state = {
+      idNum: {}
+    }
+    this.getOne = this.getStats.bind(this);
+  }
+
+  // Fetch the list on first mount
+  componentDidMount() {
+    this.getStats();
+  }
+
+  // Retrieves the list of items from the Express app
+  getStats(){
+    fetch('http://localhost:5000/api/getStats/')
+    .then(res => res.json())
+    .then(stat => { 
+        this.setState({idNum:stat})
+    }).catch(err=> console.log(err))
+  }
+
+  render() {
+    const { idNum } = this.state;
+    return (
+      <div className="App">
+        <h1>Company Stats</h1>
+          <div><br></br>
+                <div>
+                    Current Employee: {idNum["currently"]}
+                </div>
+                <div>
+                    Hired Employee over the last year: {idNum["pastYear"]}
+                </div>
+                <div>
+                    Number of Countries that we have offices in: {idNum["countries"]}
+                </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Stats;
