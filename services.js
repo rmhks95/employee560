e9d53ea67bf62feafbab0ee3df1498d790b49a26
Ryan Huse <rmhks95@ksu.edu>
2018-11-23 20:02:44 -0600

    first or last or both or number

--- services.js
+++ services.js
@@ -43,11 +43,11 @@ async function getEmployee(req, res) {
   try {
     const {name} = req.params;
     var result =""
-
-    if(name.match(/\d+/g)){
+    
+    if(!name.match(/\d+/g)){
         if(name.includes(" ")){
             var names = name.split(" ");
-            
+            console.log(names)
             //const result = await sql.query(`select * from Employee.Employee E where EmployeeId = ${id}`)
             result = await sql.query(`select
             E.EmployeeId,
@@ -65,7 +65,7 @@ async function getEmployee(req, res) {
                 INNER JOIN Employee.Office O on E.OfficeID = O.OfficeID
                 INNER JOIN Employee.Department D on E.DepartmentID = D.DepartmentID 
             where  E.FirstName= '${names[0]}' and E.LastName = '${names[1]}'`)
-        }{
+        }else{
             result = await sql.query(`select
             E.EmployeeId,
             E.FirstName,
